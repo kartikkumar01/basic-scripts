29fb85672fb4d059e2a76587227d4b881b93e715
Kartik Kumar <kartikjhaba@gmail.com>
2025-12-16 22:25:00 +0530

    Logic Building

--- 020_findSecondLargest.js
+++ 020_findSecondLargest.js
@@ -1,18 +1,41 @@
-// Given an array of integers, return the second largest number. If the array has fewer than 2 distinct(unique) numbers, return null.
-
-// function secondLargest(){}
-
-//assuming all the numbers are unique
-let numbers = [2,4,5,8,9];
-
-let firstLargestNumber = numbers[0];
-let secondLargetNumber = numbers[1];
-for(let i = 2; i<numbers.length; i++){
-    let nextNumber = numbers[i];
-    if(nextNumber > firstLargestNumber){
-        secondLargetNumber = firstLargestNumber;
-        firstLargestNumber = nextNumber;
-    }else if(nextNumber > secondLargetNumber){
-        secondLargetNumber = nextNumber;
+//Given an array of integers, return the second largest number.
+function secondLargest(numbers) {
+    if (numbers.length < 2) {
+        return null;
     }
+
+    let firstLargestNumber;
+    let secondLargestNumber = null;
+
+    if (numbers[0] > numbers[1]) {
+        firstLargestNumber = numbers[0];
+        secondLargestNumber = numbers[1];
+    } else if (numbers[1] > numbers[0]) {
+        firstLargestNumber = numbers[1];
+        secondLargestNumber = numbers[0];
+    } else {
+        firstLargestNumber = numbers[0];
+        secondLargestNumber = null;
+    }
+
+    for (let i = 2; i < numbers.length; i++) {
+        let nextNumber = numbers[i];
+
+        if (nextNumber > firstLargestNumber) {
+            secondLargestNumber = firstLargestNumber;
+            firstLargestNumber = nextNumber;
+        } else if (
+            nextNumber < firstLargestNumber &&
+            (secondLargestNumber === null || nextNumber > secondLargestNumber)
+        ) {
+            secondLargestNumber = nextNumber;
+        }
+    }
+
+    return secondLargestNumber;
 }
+
+
+const numbers = [5, 4, 7, 55, 2];
+
+console.log(secondLargest(numbers));
